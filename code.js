292b0018c9ff3e88f737c17c1c0ab0e9e7a0a7cd
David Mauricio Gallo <94572833+DavidGalloF@users.noreply.github.com>
2022-01-20 17:04:31 -0500

    envio de variables

--- code.js
+++ code.js
@@ -1,18 +1,9 @@
-
-
-
-alert(nombre);
 function captura(){
     var nombre= document.getElementById("nombre").value;
     var email= document.getElementById("email").value;
     var telefono= document.getElementById("tel").value;
+    localStorage.setItem("textvalue",nombre);
+    localStorage.setItem("textvalue2",email);
+    localStorage.setItem("textvalue3",telefono);
     location.href= "result.html";
-
-    const pnombre = document.getElementById("nombre2");
-    const pemail = document.getElementById("email2");
-    const ptel = document.getElementById("telefono2");
-
-    pnombre.textContent = nombre;
-    pemail.textContent = email;
-    ptel.textContent = telefono;
 }
